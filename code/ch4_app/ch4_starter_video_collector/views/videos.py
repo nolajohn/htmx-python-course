45608811af3ba91488e9d3d8e77e3159c930878b
nolajohn <john@johnwest.consulting>
2022-04-11 08:05:53 -0500

    Following along to the end of chapter 4

--- code/ch4_app/ch4_starter_video_collector/views/videos.py
+++ code/ch4_app/ch4_starter_video_collector/views/videos.py
@@ -1,6 +1,8 @@
 import flask
 
 from infrastructure.view_modifiers import response
+from services import video_service
+from viewmodels.videos.add_video_viewmodel import AddVideoViewModel
 from viewmodels.videos.category_viewmodel import CategoryViewModel
 from viewmodels.videos.play_viewmodel import PlayViewModel
 
@@ -19,3 +21,28 @@ def category(cat_name: str):
 def play(video_id: str):
     vm = PlayViewModel(video_id)
     return vm.to_dict()
+
+
+@blueprint.get('/videos/add/<cat_name>')
+@response(template_file='videos/partials/add_video_form.html')
+def add_get(cat_name: str):
+    vm = AddVideoViewModel(cat_name)
+    return vm.to_dict()
+
+
+@blueprint.post('/videos/add/<cat_name>')
+def add_post(cat_name: str):
+    vm = AddVideoViewModel(cat_name)
+    vm.restore_from_form()
+
+    video_service.add_video(cat_name, vm.id, vm.title, vm.author, vm.view_count)
+
+    return flask.redirect(f'/videos/category/{cat_name.lower().strip()}')
+
+
+@blueprint.get('/videos/cancel_add/<cat_name>')
+@response(template_file='videos/partials/show_add_form.html')
+def cancel_add(cat_name: str):
+    vm = AddVideoViewModel(cat_name)
+    return vm.to_dict()
+
